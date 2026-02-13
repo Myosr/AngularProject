93273c09dbb5459a2ca77fc57eaf4be16a4d0acc
maissa23-cloud <kottimaissa@gmail.com>
2026-02-13 22:59:32 +1300

    update3

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+import { DashboardAdminComponent } from './dashboard-admin/dashboard-admin.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    RegisterComponent
+    RegisterComponent,
+    DashboardAdminComponent
   ],
   imports: [
     BrowserModule,
